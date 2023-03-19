11f628293af161ff6a3fe972ac54c6bf56b09656
אורטל חנה < @K23>
2023-03-19 15:57:10 +0200

    another step

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from flask_cors import CORS,cross_origin
 from flask_restful import Api
 # from user_actions import UsersActions
 import json
+from flask import Flask, request, jsonify
+from datetime import datetime, timedelta, timezone
+import json
+from flask import Flask, request, jsonify
+from datetime import datetime, timedelta, timezone
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
+                               unset_jwt_cookies, jwt_required, JWTManager 
 # from werkzeug import secure_filename
 # from db_manger import DBManger
 from dal.data_objects.user_crud import UserCRUD
@@ -16,6 +23,9 @@ users = my_data_base["users"]
 
 app = flask.Flask(__name__)
 CORS(app)
+app.config["JWT_SECRET_KEY"] = "please-remember-to-change-me"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+jwt = JWTManager(app)
 # @app.route('/')
 def hello():
     response=flask.jsonify()
@@ -79,6 +89,7 @@ def add_students():
     # data.save(secure_filename(data.filename))
     return flask.jsonify({"hee":"only connection try"})
 #api.add_resource(UsersActions,"/users_actions/<string:user_id>/<string:user_name>")
+
 @app.route("/get_all_users")
 def get_all():
     user=UserCRUD()
@@ -94,5 +105,49 @@ def get_all():
 # def delet():
 #     user=UserCRUD()
     # final=user.
+    
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            data = response.get_json()
+            if type(data) is dict:
+                data["access_token"] = access_token 
+                response.data = json.dumps(data)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
+
+@app.route('/token', methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test" :
+        return {"message": "Wrong email or password"}, 401
+
+    access_token = create_access_token(identity=email)
+    response = {"access_token":access_token}
+    return response
+    # return jsonify({"hi":"success"})
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+
+@app.route('/profile')
+def my_profile():
+    response_body = {
+        "name": "Nagato",
+        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+    }
+    return response_body
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='8000', debug=True)
