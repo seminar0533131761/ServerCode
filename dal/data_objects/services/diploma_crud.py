381b7c00d9dbd3d11823e716056cb57639b4f0ef
senminar0533131761 <hand2b2@mbjcomp.org.il>
2023-05-01 20:33:38 +0300

    1/5

--- dal/data_objects/services/diploma_crud.py
+++ dal/data_objects/services/diploma_crud.py
@@ -17,5 +17,6 @@ class DiplomaCRUD(ICRUD):
         pass
     def get_async(self,id):
         tmp_diploma=self.diplomas.find_one({"_id":id})
-        self.diploma=Diploma(tmp_diploma["_id"],tmp_diploma["math"])
+        # self.diploma=Diploma(tmp_diploma["_id"],tmp_diploma["math"],tmp_diploma["english"],tmp_diploma["torah"],tmp_diploma["sciences"],tmp_diploma["grammar"],tmp_diploma["history"],tmp_diploma["trend"])
+        self.diploma=Diploma("214088999",9,5,8,9,10,7,"math")
         return self.diploma
--- dal/data_objects/services/student_crud.py
+++ dal/data_objects/services/student_crud.py
@@ -7,28 +7,26 @@ class StudentCrud(ICRUD):
         self.student = {}
     def create_async(self, obj):
         # tmp_user = self.users.insert_one(obj)
-        # self.user = User(tmp_user.user_name, tmp_user._id, tmp_user.permmision)
+        # self.user = User(tmp_user.user_name, tmp_user._id, tmp_user.permission)
         # return self.user
         pass
 
     def delete_async(self, id):
         # tmp_user = self.users.delete_one({"_id": id})
-        # self.user = User(tmp_user.user_name, tmp_user._id, tmp_user.permmision)
+        # self.user = User(tmp_user.user_name, tmp_user._id, tmp_user.permission)
         # return self.user
         pass
 
     def update_async(self, id, permmision):
-        # tmp_user = self.users.update_one({"_id": id}, {"permmision": permmision})
-        # self.user = User(tmp_user.user_name, tmp_user._id, tmp_user.permmision)
+        # tmp_user = self.users.update_one({"_id": id}, {"permission": permission})
+        # self.user = User(tmp_user.user_name, tmp_user._id, tmp_user.permission)
         # return self.user
         pass
 
     def get_async(self, _id):
         tmp_student = self.students.find_one({"_id": 214088999})
-        print (self)
-        print (self.students)
-        print (tmp_student["_id"])
-        self.student = Student(tmp_student["_id"], tmp_student["first_name"], tmp_student["last_name"],tmp_student["phone"])
+        # self.student = Student(tmp_student["_id"], tmp_student["first_name"], tmp_student["last_name"],tmp_student["phone"])
+        self.student=Student("21","chani","orthal","054648")
         return self.student
     async def get_all_async(self):
         pass
